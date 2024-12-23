f501de1069c1ccbb8bbfad5eddd9648143a24b7e
1-Me-Abhi <sudo.apt.abhi@gmail.com>
2024-12-24 03:37:37 +0530

    na

---
+++ Javascript/Javascript varabiles/script.js
@@ -0,0 +1,29 @@
+// console.log("Hey Abhi")
+// var a=5;
+// var b= 10;
+// var c=a+b;
+// console.log(c)
+// let age=20;
+// console.log("Age is :"+age)
+// // const userInput = prompt();
+// // console.log(userInput)
+
+// if(age=>18){
+//     console.log("The person can Drive")
+// }
+// else{
+//     console.log("The person is not allowed to Drive")
+// }
+console.log("Basic calculator")
+let a=2;
+let b=3;
+let c="-"
+if(c=="+"){
+    console.log("Sum is : "+(a+b))
+}
+else if(c=="-"){
+    console.log("sub is : "+(a-b))
+}
+else{
+    console.log("eror")
+}
