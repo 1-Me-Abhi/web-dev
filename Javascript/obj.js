4efc81a9e432ec7f9546f07671aee20becc5bb3c
Abhishek Kumar <kumarabhi45380@gmail.com>
2025-08-10 18:16:45 +0530

    tyr catch

--- Javascript/obj.js
+++ Javascript/obj.js
@@ -14,3 +14,12 @@ console.log(Abhi);
 let Abhi2=Object.assign({},Abhi);
 Abhi2.age=23;
 console.log(Abhi2);
+try{
+    console.log(Abhi2.city);
+    console.log(Abhi2.country);
+}catch(error){
+    console.log(error);
+}
+finally{
+    console.log("Finally block executed");
+}
