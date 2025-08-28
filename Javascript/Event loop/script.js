40d5741ad89d3f8b45c5c68be02cada59ecafdbd
Abhishek Kumar <sudo.apt.abhi@gmail.com>
2025-08-28 16:28:39 +0530

    adding new files which learn today

---
+++ Javascript/Event loop/script.js
@@ -0,0 +1,5 @@
+console.log("Hii Abhi")
+let firstPromish=new Promise((resolve, reject) => {
+    console.log("working")
+})
+console.log("hello")
---
+++ Javascript/Event loop/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+console.log("Hii Abhi")
+let firstPromish=new Promise((resolve, reject) => {
+    console.log("working")
+    resolve(100)
+})
